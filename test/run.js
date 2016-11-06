597edda490862cdeb8d3ef9502a7cce49271a7a7
vitalets <vitalets@yandex-team.ru>
2016-11-06 21:29:17 +0300

    selftest: use collector in normal flow

--- test/run.js
+++ test/run.js
@@ -5,8 +5,8 @@ global.fn = require('./calls');
 
 const config = {
   //files: './test/specs/*.js'
-  //files: './test/specs/normal-flow.test.js'
-  files: './test/specs/error-flow.test.js'
+  files: './test/specs/normal-flow.test.js'
+  //files: './test/specs/error-flow.test.js'
 };
 new Sheeva(config).run();
 
--- test/specs/normal-flow.test.js
+++ test/specs/normal-flow.test.js
@@ -6,58 +6,106 @@ describe('normal flow', () => {
   });
 
   it('should run it without describe', () => {
-    new Sheeva({files: './test/data/it.js'}).run();
-    expect(fn.calls, 'to equal', [
-      'test 1',
-      'test 2',
+    const sheeva = new Sheeva({files: './test/data/it.js'});
+    sheeva.run();
+    expect(sheeva.collector.log, 'to equal', [
+      'SUITE_START root',
+      'TEST_START test 1',
+      'TEST_END test 1',
+      'TEST_START test 2',
+      'TEST_END test 2',
+      'SUITE_END root',
     ])
   });
 
   it('should run it and all hooks within describe', () => {
-    new Sheeva({files: './test/data/it-hooks.js'}).run();
-    expect(fn.calls, 'to equal', [
-      'before 1',
-      'before 2',
-      'beforeEach 1',
-      'beforeEach 2',
-      'test 1',
-      'afterEach 1',
-      'afterEach 2',
-      'beforeEach 1',
-      'beforeEach 2',
-      'test 2',
-      'afterEach 1',
-      'afterEach 2',
-      'after 1',
-      'after 2',
+    const sheeva = new Sheeva({files: './test/data/it-hooks.js'});
+    sheeva.run();
+    expect(sheeva.collector.log, 'to equal', [
+      'SUITE_START root',
+      'HOOK_START root before 0',
+      'HOOK_END root before 0',
+      'HOOK_START root before 1',
+      'HOOK_END root before 1',
+      'HOOK_START root beforeEach 0',
+      'HOOK_END root beforeEach 0',
+      'HOOK_START root beforeEach 1',
+      'HOOK_END root beforeEach 1',
+      'TEST_START test 1',
+      'TEST_END test 1',
+      'HOOK_START root afterEach 0',
+      'HOOK_END root afterEach 0',
+      'HOOK_START root afterEach 1',
+      'HOOK_END root afterEach 1',
+      'HOOK_START root beforeEach 0',
+      'HOOK_END root beforeEach 0',
+      'HOOK_START root beforeEach 1',
+      'HOOK_END root beforeEach 1',
+      'TEST_START test 2',
+      'TEST_END test 2',
+      'HOOK_START root afterEach 0',
+      'HOOK_END root afterEach 0',
+      'HOOK_START root afterEach 1',
+      'HOOK_END root afterEach 1',
+      'HOOK_START root after 0',
+      'HOOK_END root after 0',
+      'HOOK_START root after 1',
+      'HOOK_END root after 1',
+      'SUITE_END root',
     ]);
   });
 
   it('should run it with describe', () => {
-    new Sheeva({files: './test/data/describe-it.js'}).run();
-    expect(fn.calls, 'to equal', [
-      'test 1',
-      'test 2'
+    const sheeva = new Sheeva({files: './test/data/describe-it.js'});
+    sheeva.run();
+    expect(sheeva.collector.log, 'to equal', [
+      'SUITE_START root',
+      'SUITE_START suite',
+      'TEST_START test 1',
+      'TEST_END test 1',
+      'TEST_START test 2',
+      'TEST_END test 2',
+      'SUITE_END suite',
+      'SUITE_END root',
     ])
   });
 
   it('should run it and all hooks within describe', () => {
-    new Sheeva({files: './test/data/describe-it-hooks.js'}).run();
-    expect(fn.calls, 'to equal', [
-      'before 1',
-      'before 2',
-      'beforeEach 1',
-      'beforeEach 2',
-      'test 1',
-      'afterEach 1',
-      'afterEach 2',
-      'beforeEach 1',
-      'beforeEach 2',
-      'test 2',
-      'afterEach 1',
-      'afterEach 2',
-      'after 1',
-      'after 2',
+    const sheeva = new Sheeva({files: './test/data/describe-it-hooks.js'});
+    sheeva.run();
+    expect(sheeva.collector.log, 'to equal', [
+      'SUITE_START root',
+      'SUITE_START suite',
+      'HOOK_START suite before 0',
+      'HOOK_END suite before 0',
+      'HOOK_START suite before 1',
+      'HOOK_END suite before 1',
+      'HOOK_START suite beforeEach 0',
+      'HOOK_END suite beforeEach 0',
+      'HOOK_START suite beforeEach 1',
+      'HOOK_END suite beforeEach 1',
+      'TEST_START test 1',
+      'TEST_END test 1',
+      'HOOK_START suite afterEach 0',
+      'HOOK_END suite afterEach 0',
+      'HOOK_START suite afterEach 1',
+      'HOOK_END suite afterEach 1',
+      'HOOK_START suite beforeEach 0',
+      'HOOK_END suite beforeEach 0',
+      'HOOK_START suite beforeEach 1',
+      'HOOK_END suite beforeEach 1',
+      'TEST_START test 2',
+      'TEST_END test 2',
+      'HOOK_START suite afterEach 0',
+      'HOOK_END suite afterEach 0',
+      'HOOK_START suite afterEach 1',
+      'HOOK_END suite afterEach 1',
+      'HOOK_START suite after 0',
+      'HOOK_END suite after 0',
+      'HOOK_START suite after 1',
+      'HOOK_END suite after 1',
+      'SUITE_END suite',
+      'SUITE_END root',
     ]);
   });
 
