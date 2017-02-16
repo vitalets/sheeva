e4125bc7045d08da3b47263b4f9825e7e02ac5e4
vitalets <vitalets@yandex-team.ru>
2017-02-16 08:34:15 +0300

    queue: refactor to private fields

--- src/executer/queues/queue.js
+++ src/executer/queues/queue.js
@@ -15,30 +15,39 @@ module.exports = class Queue {
    * @param {Array} tests
    */
   constructor(tests) {
-    // todo: use _
     assertTests(tests);
-    this.tests = tests;
-    this.suite = tests[0].parents[0];
-    this.isRunning = false;
-    this.currentIndex = -1;
-    this.currentTest = null;
-    this.nextTest = this.tests[0];
-    this.suiteStack = [];
-    this.promised = new utils.Promised();
+    this._tests = tests;
+    this._suite = tests[0].parents[0];
+    this._isRunning = false;
+    this._currentIndex = -1;
+    this._currentTest = null;
+    this._nextTest = this._tests[0];
+    this._suiteStack = [];
+    this._caller = null;
+    this._promised = new utils.Promised();
+  }
+
+  get tests() {
+    return this._tests;
+  }
+
+  get suite() {
+    return this._suite;
   }
 
   get env() {
-    return this.suite.env;
+    return this._suite.env;
   }
 
   /**
-   * Remaining tests count depends on if queue is running or not, because queue runs hooks considering next test,
+   * Remaining tests count depends on if queue is running or not,
+   * because queue runs hooks considering next test,
    * and if this test will be splitted we will got error.
    *
    * @returns {Number}
    */
   getRemainingTestsCount() {
-    return this.isRunning ? this.tests.length - this.currentIndex - 2 : this.tests.length;
+    return this._isRunning ? this._tests.length - this._currentIndex - 2 : this._tests.length;
   }
 
   /**
@@ -48,29 +57,39 @@ module.exports = class Queue {
    * @returns {Promise}
    */
   run(caller) {
-    return this.promised.call(() => {
-      this.isRunning = true;
-      this.caller = caller;
-      this.next()
-        .catch(e => this.promised.reject(e));
+    return this._promised.call(() => {
+      this._isRunning = true;
+      this._caller = caller;
+      this._next()
+        .catch(e => this._promised.reject(e));
     });
+  }
+
+  /**
+   * Splits out another queue out of this
+   *
+   * @param {Number} fromIndex
+   */
+  split(fromIndex) {
+    const tests = this._tests.splice(fromIndex);
+    return new Queue(tests);
   }
 
   /**
    * Process next item in queue
    */
-  next() {
+  _next() {
     return Promise.resolve()
-      .then(() => this.moveToNextTest())
+      .then(() => this._moveToNextTest())
       .then(() => {
-        if (this.currentTest) {
-          return this.caller.callTestWithEachHooks(this.suiteStack, this.currentTest)
+        if (this._currentTest) {
+          return this._caller.callTestWithEachHooks(this._suiteStack, this._currentTest)
             .then(
-              () => this.next(),
-              () => this.handleHookError()
+              () => this._next(),
+              () => this._handleHookError()
             )
         } else {
-          this.promised.resolve();
+          this._promised.resolve();
         }
       })
   }
@@ -80,24 +99,24 @@ module.exports = class Queue {
    * If there is suite change, call needed `before|after` hooks.
    *
    */
-  moveToNextTest() {
-    return this.isSuiteChange()
-      ? this.handleSuiteChange()
-      : this.incrementIndex();
+  _moveToNextTest() {
+    return this._isSuiteChange()
+      ? this._handleSuiteChange()
+      : this._incrementIndex();
   }
 
-  isSuiteChange() {
-    return !this.currentTest || !this.nextTest || this.currentTest.parent !== this.nextTest.parent;
+  _isSuiteChange() {
+    return !this._currentTest || !this._nextTest || this._currentTest.parent !== this._nextTest.parent;
   }
 
-  handleSuiteChange() {
-    const commonSuite = this.getCommonSuite();
-    return this.caller.callAfter(this.suiteStack, commonSuite)
+  _handleSuiteChange() {
+    const commonSuite = this._getCommonSuite();
+    return this._caller.callAfter(this._suiteStack, commonSuite)
       .then(() => {
-        this.incrementIndex();
-        if (this.currentTest) {
-          return this.caller.callBefore(this.suiteStack, this.currentTest)
-            .catch(() => this.handleHookError());
+        this._incrementIndex();
+        if (this._currentTest) {
+          return this._caller.callBefore(this._suiteStack, this._currentTest)
+            .catch(() => this._handleHookError());
         }
       })
   }
@@ -105,13 +124,13 @@ module.exports = class Queue {
   /**
    * Increments currentIndex and set currentTest / nextTest
    */
-  incrementIndex() {
-    if (this.currentIndex > this.tests.length) {
-      throw new Error(`Going out of queue: ${this.suite.name}`);
+  _incrementIndex() {
+    if (this._currentIndex > this._tests.length) {
+      throw new Error(`Going out of queue: ${this._suite.name}`);
     }
-    this.currentIndex++;
-    this.currentTest = this.tests[this.currentIndex];
-    this.nextTest = this.tests[this.currentIndex + 1];
+    this._currentIndex++;
+    this._currentTest = this._tests[this._currentIndex];
+    this._nextTest = this._tests[this._currentIndex + 1];
   }
 
   /**
@@ -119,10 +138,10 @@ module.exports = class Queue {
    *
    * @param {Suite} suite
    */
-  incrementIndexToLastTestInSuite(suite) {
-    const positionInStack = this.suiteStack.findIndex(s => s === suite);
-    while (!this.isSuiteEnd(positionInStack)) {
-      this.incrementIndex();
+  _incrementIndexToLastTestInSuite(suite) {
+    const positionInStack = this._suiteStack.findIndex(s => s === suite);
+    while (!this._isSuiteEnd(positionInStack)) {
+      this._incrementIndex();
     }
   }
 
@@ -135,30 +154,20 @@ module.exports = class Queue {
    * @param {Number} endSuitePos
    * @returns {boolean}
    */
-  isSuiteEnd(endSuitePos) {
-    if (this.isSuiteChange()) {
-      const commonSuite = this.getCommonSuite();
-      const commonSuitePos = this.suiteStack.findIndex(suite => suite === commonSuite);
+  _isSuiteEnd(endSuitePos) {
+    if (this._isSuiteChange()) {
+      const commonSuite = this._getCommonSuite();
+      const commonSuitePos = this._suiteStack.findIndex(suite => suite === commonSuite);
       return commonSuitePos < endSuitePos;
     } else {
       return false;
     }
   }
 
-  handleHookError() {
-    this.incrementIndexToLastTestInSuite(this.caller.errorSuite);
+  _handleHookError() {
+    this._incrementIndexToLastTestInSuite(this._caller.errorSuite);
     // all needed `after` hooks will be called in this.next()
-    return this.next();
-  }
-
-  /**
-   * Splits out another queue out of this
-   *
-   * @param {Number} fromIndex
-   */
-  split(fromIndex) {
-    const tests = this.tests.splice(fromIndex);
-    return new Queue(tests);
+    return this._next();
   }
 
   /**
@@ -168,25 +177,25 @@ module.exports = class Queue {
    *
    * @returns {Suite}
    */
-  getCommonSuite() {
-    if (!this.currentTest || !this.nextTest) {
+  _getCommonSuite() {
+    if (!this._currentTest || !this._nextTest) {
       return null;
     }
 
     const maxLength = Math.max(
-      this.currentTest.parents.length,
-      this.nextTest.parents.length
+      this._currentTest.parents.length,
+      this._nextTest.parents.length
     );
     for (let i = 0; i < maxLength; i++) {
-      const p1 = this.currentTest.parents[i];
-      const p2 = this.nextTest.parents[i];
+      const p1 = this._currentTest.parents[i];
+      const p2 = this._nextTest.parents[i];
       if (p1 != p2) {
-        return this.currentTest.parents[i - 1];
+        return this._currentTest.parents[i - 1];
       }
     }
 
     // todo: warn
-    return this.currentTest.parent;
+    return this._currentTest.parent;
   }
 };
 
