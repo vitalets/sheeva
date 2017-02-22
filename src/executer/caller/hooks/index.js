692f2ebf7bc86027d28026ff5085220c37dac5dc
vitalets <vitalets@yandex-team.ru>
2017-02-22 17:49:07 +0300

    caller: comments

--- src/executer/caller/hooks/index.js
+++ src/executer/caller/hooks/index.js
@@ -2,5 +2,14 @@
  * Classes for calling suite-level hooks (before/after) and test-level hooks (beforeEach/afterEach)
  */
 
+/**
+ * Calls suite level hooks (before / after).
+ * Used in Queue.
+ */
 exports.SuiteHooksCaller = require('./suite-hooks');
+
+/**
+ * Calls test level hooks (beforeEach / afterEach).
+ * Used in caller/test.js
+ */
 exports.TestHooksCaller = require('./test-hooks');
--- src/executer/caller/index.js
+++ src/executer/caller/index.js
@@ -2,5 +2,14 @@
  * Classes for calling hooks and test
  */
 
+/**
+ * Calls suite level hooks (before / after).
+ * Used in Queue.
+ */
 exports.SuiteHooksCaller = require('./hooks').SuiteHooksCaller;
+
+/**
+ * Calls test fn with level hooks (beforeEach / afterEach).
+ * Used in Queue.
+ */
 exports.TestCaller = require('./test');
