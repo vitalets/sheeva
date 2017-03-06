7c01ad80b07331bb7effa4430eb7c08d455a1ec8
vitalets <vitalets@yandex-team.ru>
2017-03-06 22:59:55 +0300

    selftest: add env events  test

--- test/specs/config/envs.test.js
+++ test/specs/config/envs.test.js
@@ -14,32 +14,57 @@ describe('config envs', () => {
       },
     };
     const report = run(`
-        describe('suite', () => {
-          it('test 0', noop);
-          it('test 1', noop);
-        });
-      `, {config});
+      describe('suite', () => {
+        it('test 0', noop);
+        it('test 1', noop);
+      });
+    `, {config});
 
     return expectResolve(report, {
-        env1: {
-          session0: [
-            'SESSION_START 0',
-            'TEST_END test 0',
-            'TEST_END test 1',
-            'SESSION_END 0'
-          ]
-        },
-        env2: {
-          session0: [
-            'SESSION_START 0',
-            'TEST_END test 0',
-            'TEST_END test 1',
-            'SESSION_END 0'
-          ]
-        }
+      env1: {
+        session0: [
+          'SESSION_START 0',
+          'TEST_END test 0',
+          'TEST_END test 1',
+          'SESSION_END 0'
+        ]
+      },
+      env2: {
+        session0: [
+          'SESSION_START 0',
+          'TEST_END test 0',
+          'TEST_END test 1',
+          'SESSION_END 0'
+        ]
       }
-    );
+    });
   });
+
+  it('should emit env events correctly', run => {
+    const config = {
+      createEnvs: function () {
+        return [
+          {id: 'env1'},
+          {id: 'env2'},
+        ];
+      },
+    };
+    const report = run(`
+      describe('suite', () => {
+        it('test 0', noop);
+      });
+    `, {config, flat: true, include: ['ENV', 'TEST_END']});
+
+    return expectResolve(report, [
+      'ENV_START env1',
+      'TEST_END test 0',
+      'ENV_END env1',
+      'ENV_START env2',
+      'TEST_END test 0',
+      'ENV_END env2'
+    ]);
+  });
+
 
   it('should fail in case of no envs', run => {
     const config = {
