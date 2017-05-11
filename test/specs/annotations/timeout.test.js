9bbdb761ccd82ff2ff301f0df3e47b4d59c1edfa
vitalets <vitalets@yandex-team.ru>
2017-05-11 13:14:38 +0300

    selftest: increase timeout from 10 to 20

--- test/specs/annotations/timeout.test.js
+++ test/specs/annotations/timeout.test.js
@@ -4,46 +4,46 @@ describe('annotation: timeout', () => {
   it('should fail after test timeout', run => {
     const result = run(`
       describe('suite 1', () => {
-        $timeout(10);
-        it('test 0', () => sleep(20));
+        $timeout(20);
+        it('test 0', () => sleep(30));
         it('test 1', () => sleep(1));
       });
     `);
 
     return expectResolve(result, [
-      'TEST_END test 0 Timeout 10 ms exceeded',
+      'TEST_END test 0 Timeout 20 ms exceeded',
       'TEST_END test 1'
     ]);
   });
 
   it('should fail after suite timeout', run => {
     const result = run(`
-      $timeout(10);
+      $timeout(20);
       describe('suite 1', () => {
-        it('test 0', () => sleep(20));
+        it('test 0', () => sleep(30));
         it('test 1', () => sleep(1));
       });
     `);
 
     return expectResolve(result, [
-      'TEST_END test 0 Timeout 10 ms exceeded',
+      'TEST_END test 0 Timeout 20 ms exceeded',
       'TEST_END test 1'
     ]);
   });
 
   it('should fail after global timeout', run => {
     const config = {
-      timeout: 10
+      timeout: 20
     };
     const result = run(`
       describe('suite 1', () => {
-        it('test 0', () => sleep(20));
+        it('test 0', () => sleep(30));
         it('test 1', () => sleep(1));
       });
     `, {config});
 
     return expectResolve(result, [
-      'TEST_END test 0 Timeout 10 ms exceeded',
+      'TEST_END test 0 Timeout 20 ms exceeded',
       'TEST_END test 1'
     ]);
   });
